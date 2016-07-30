d55a537cfa8c79ea6b1a4f4cba776051b83f7433
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-07-29 22:43:34 -0700

    added image filter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,68 @@ var angular = require('angular');
 var myApp = angular.module('myApp',[]);
 
 
-myApp.controller('main',function($scope){
-	$scope.name = 'nick';
+myApp.controller('main',function($scope,$filter){
+	$scope.skills = ['All','Javascript','CSS'];
+	$scope.active = 0;
+	$scope.list = [
+	{
+		tags:['All','CSS'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','CSS'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','CSS'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','Javascript'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','Javascript'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','Javascript'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','Javascript'],
+		url:'http://placehold.it/250x250'
+	},
+	{
+		tags:['All','Javascript','CSS'],
+		url:'http://placehold.it/250x250'
+	}
+	];
+
+	$scope.second = $scope.list;
+
+	$scope.toggleActive = function(index,skill) {
+	
+		$scope.active = index;
+
+		$scope.filterList(skill)
+	}
+
+	$scope.filterList = function(skill) {
+		$scope.second = [];
+		$scope.list.forEach(function(item){
+			return item.tags.forEach(function(tag){
+				if(skill===tag){
+					$scope.second.push(item)
+					return true;
+				}
+			})
+		})
+
+		console.log($scope.second)
+	};
+
+		
+	
 })
 
